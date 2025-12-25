9fcf9717e5122f2e0fc5d5ac07469343d90f2f43
JungmoKoo <gooj978@gmail.com>
2025-12-26 00:33:49 +0900

    update project info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,14 +23,14 @@ const Projects = () => {
         },
         {
             title: "[NAVER LABS] Rookie2",
-            type: "Robotics/Computer Vision",
+            type: "Computer Vision / Robotics",
             image: "https://img.youtube.com/vi/yL6RieTPRG8/0.jpg",
             link: "https://www.youtube.com/watch?v=yL6RieTPRG8",
             isVideo: true
         },
         {
             title: "[NAVER LABS] Perception",
-            type: "Robotics/Computer Vision",
+            type: "Computer Vision / Robotics",
             // image: "https://jungmokoo.github.io/penebev/assets/images/compare_1_cam2bev.png",
             image: penebevImg,
             link: "https://jungmokoo.github.io/penebev",
@@ -53,7 +53,7 @@ const Projects = () => {
         },
         {
             title: "DJI developer challenge",
-            type: "Competition",
+            type: "Computer Vision / Robotics",
             image: djiImg,
             link: "https://www.youtube.com/watch?v=W1y--cfUjOs&t=502s",
             isVideo: true
@@ -67,14 +67,14 @@ const Projects = () => {
         },
         {
             title: "JEROS (IEEE Spectrum)",
-            type: "Robotics / Computer Vision",
+            type: "Computer Vision / Robotics",
             image: "https://spectrum.ieee.org/media-library/image.jpg?id=25573686&width=1200&height=814",
             link: "https://spectrum.ieee.org/jeros-jellyfish-elimination-robotic-swarm",
             isVideo: false
         },
         {
             title: "JEROS vision",
-            type: "Robotics / Computer Vision",
+            type: "Computer Vision / Robotics",
             image: "https://img.youtube.com/vi/sZRiNSmyBcY/0.jpg",
             link: "https://www.youtube.com/watch?v=sZRiNSmyBcY",
             isVideo: true
@@ -88,7 +88,7 @@ const Projects = () => {
         },
         {
             title: "국제로봇컨테스트 - 국무총리상",
-            type: "Competition",
+            type: "Computer Vision / Robotics",
             image: "https://img1.newsis.com/2012/10/28/NISI20121028_0007245550_web.jpg",
             link: "https://www.newsis.com/view/NISX20121028_0011559983#:~:text=%EB%98%90%ED%95%9C%20%EA%B5%AD%EB%AC%B4%EC%B4%9D%EB%A6%AC%EC%83%81%EC%9D%80%20%EB%8F%99%EC%95%84%EB%8C%80%20%EB%8D%B0%EC%9A%B0%EC%8A%A4%ED%8C%80(%EC%A7%80%EB%8A%A5%ED%98%95%20SoC%EB%A1%9C%EB%B4%87%EC%9B%8C%20SoC%20%ED%83%9C%EA%B6%8C%EB%A1%9C%EB%B4%87%20%EB%B6%80%EB%AC%B8)%2C%20%EA%B2%BD%ED%9D%AC%EB%8C%80%20%EA%BF%80%ED%8C%80(%ED%83%90%EC%82%AC%EB%A1%9C%EB%B4%87%EB%8C%80%ED%9A%8C%20%ED%83%90%EC%82%AC%EB%A1%9C%EB%B4%87%20%EB%B6%80%EB%AC%B8)%EC%97%90%20%EB%8F%8C%EC%95%84%EA%B0%94%EB%8B%A4.",
             isVideo: false
